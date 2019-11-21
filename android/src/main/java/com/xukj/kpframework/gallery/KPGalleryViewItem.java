a0ccd63c32609fa1fbbd55acc6b6e2949a168edc
xukj <xukj002@forsealife.com>
2019-11-21 10:30:13 +0800

    support androidx

--- android/src/main/java/com/xukj/kpframework/gallery/KPGalleryViewItem.java
+++ android/src/main/java/com/xukj/kpframework/gallery/KPGalleryViewItem.java
@@ -4,7 +4,7 @@ import android.content.Context;
 import android.graphics.BitmapFactory;
 import android.graphics.PointF;
 import android.net.Uri;
-import android.support.annotation.Nullable;
+import androidx.annotation.Nullable;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
 import android.util.Log;
--- android/src/main/java/com/xukj/kpframework/gallery/KPGalleryViewManager.java
+++ android/src/main/java/com/xukj/kpframework/gallery/KPGalleryViewManager.java
@@ -3,7 +3,7 @@ package com.xukj.kpframework.gallery;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
+import androidx.annotation.Nullable;
 import android.util.Log;
 
 import com.facebook.react.bridge.ReadableArray;
